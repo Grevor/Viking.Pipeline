7d0862a13834cc3380bcd8ab06045a69e221554a
Erik Nyström <grevor.priest@gmail.com>
2019-08-31 09:16:22 +0200

    Added WPF project for WPF-related stages.

---
+++ Viking.Pipeline.FrameworkWpf/DispatcherUtilities.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Threading;
+using System.Windows;
+
+namespace Viking.Pipeline.Wpf
+{
+    public static class DispatcherUtilities
+    {
+
+        public static Dispatcher DefaultDispatcher => Application.Current?.Dispatcher ?? Dispatcher.CurrentDispatcher;
+    }
+}
