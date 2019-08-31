86dbf7a884e73f7903336fff06e43f51f23031cd
Erik Nyström <grevor.priest@gmail.com>
2019-08-31 19:33:42 +0100

    Added delta pipeline stage.

---
+++ Viking.Pipeline.Tests/DeltaPipelineStageTests.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Viking.Pipeline.Tests
+{
+    [TestFixture]
+    public class DeltaPipelineStageTests
+    {
+        [Test]
+        public void ExceptionOnNullInputToConstructor()
+        {
+            var valid = 1.AsPipelineConstant();
+            PipelineAssert.NullArgumentException(() => new DeltaPipelineStage<int, int>(null, Extractor, valid, 1), "name");
+            PipelineAssert.NullArgumentException(() => new DeltaPipelineStage<int, int>("", null, valid, 1), "extractor");
+            PipelineAssert.NullArgumentException(() => new DeltaPipelineStage<int, int>("", Extractor, null, 1), "input");
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(-20)]
+        public void InitialDeltaValueIsGivenBeforeAnyInvalidations(int value)
+        {
+            var sut = new DeltaPipelineStage<int, int>("", Extractor, 1.AsPipelineConstant(), value);
+            PipelineAssert.Value(sut, value);
+        }
+
+        [TestCase(10, 11, 1)]
+        [TestCase(-10, 34, 44)]
+        [TestCase(1, -2, -3)]
+        public void ExtractorIsUsedToCalculateNewDelta(int old, int @new, int expected)
+        {
+            var input = new AssignablePipelineStage<int>("", old);
+            var sut = new DeltaPipelineStage<int, int>("", Extractor, input, expected - 1);
+            var test = sut.AttachTestStage();
+
+            PipelineAssert.Value(sut, expected - 1);
+
+            input.SetValue(@new);
+            test.AssertInvalidations(1);
+            PipelineAssert.Value(sut, expected);
+        }
+
+        [Test]
+        public void InputIsAddedAsDependency()
+        {
+            var input = 1.AsPipelineConstant();
+            var sut = new DeltaPipelineStage<int, int>("", Extractor, input, -2);
+
+            PipelineAssert.DependentOn(sut, input);
+        }
+
+
+
+        private static int Extractor(int old, int @new) => @new - old;
+    }
+}
---
+++ Viking.Pipeline/DeltaPipelineStage.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Viking.Pipeline
+{
+    /// <summary>
+    /// Delegate extracting some change from an old value to a new one.
+    /// </summary>
+    /// <typeparam name="TInput">The data to look for changes in.</typeparam>
+    /// <typeparam name="TDelta">The delta.</typeparam>
+    /// <param name="oldValue">The old value.</param>
+    /// <param name="newValue">The new value.</param>
+    /// <returns>The delta.</returns>
+    public delegate TDelta DeltaExtractor<in TInput, out TDelta>(TInput oldValue, TInput newValue);
+
+    /// <summary>
+    /// Supports getting the delta of a pipeline stage. Eager stage.
+    /// </summary>
+    /// <typeparam name="TInput">The input type.</typeparam>
+    /// <typeparam name="TOutput">The delta type.</typeparam>
+    public class DeltaPipelineStage<TInput, TOutput> : IPipelineStage<TOutput>
+    {
+        /// <summary>
+        /// Creates a new <see cref="DeltaPipelineStage{TInput, TOutput}"/> with the specified name, extractor and input.
+        /// </summary>
+        /// <param name="name">The name of this stage.</param>
+        /// <param name="deltaExtractor">The function used to extract a delta.</param>
+        /// <param name="input">The pipeline stage which to extract delta from.</param>
+        /// <param name="initial">The initial delta value.</param>
+        public DeltaPipelineStage(
+            string name,
+            DeltaExtractor<TInput, TOutput> deltaExtractor,
+            IPipelineStage<TInput> input,
+            TOutput initial)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            DeltaExtractor = deltaExtractor ?? throw new ArgumentNullException(nameof(deltaExtractor));
+            Input = input ?? throw new ArgumentNullException(nameof(input));
+            CurrentDelta = initial;
+            OldValue = input.GetValue();
+            this.AddDependencies(input);
+        }
+
+        public string Name { get; }
+        /// <summary>
+        /// Gets the function used to extract the delta.
+        /// </summary>
+        public DeltaExtractor<TInput, TOutput> DeltaExtractor { get; }
+        /// <summary>
+        /// Gets the pipeline stage which to extract delta for.
+        /// </summary>
+        public IPipelineStage<TInput> Input { get; }
+        private TOutput CurrentDelta { get; set; }
+        private TInput OldValue { get; set; }
+
+        public TOutput GetValue() => CurrentDelta;
+
+        public void OnInvalidate(IPipelineInvalidator invalidator)
+        {
+            var newValue = Input.GetValue();
+            CurrentDelta = DeltaExtractor(OldValue, newValue);
+            OldValue = newValue;
+            invalidator.InvalidateAllDependentStages(this);
+        }
+    }
+}
