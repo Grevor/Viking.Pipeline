c9728e3058bceef9ae2fd3603125f3de2c7eb84d
Erik Nyström <grevor.priest@gmail.com>
2019-09-10 22:08:25 +0200

    Added proposal of hierarchical suspenders.

---
+++ Viking.Pipeline.Tests/Patterns/HierarchicalSuspenderNodeTests.cs
@@ -0,0 +1,127 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using Viking.Pipeline.Patterns;
+
+namespace Viking.Pipeline.Tests.Patterns
+{
+    [TestFixture]
+    public class HierarchicalSuspenderNodeTests
+    {
+        [Test]
+        public void InitialNodeHasTheInputAsOutput()
+        {
+            var stage = PipelineTestHelper.Assignable(PipelineSuspensionState.Resume);
+            var sut = new HierarchicalSuspenderNode(stage);
+
+            Assert.AreSame(stage, sut.Output);
+        }
+
+        [TestCase(
+            PipelineSuspensionState.Resume, 
+            new[] { PipelineSuspensionState.ResumeWithoutPendingInvalidates, PipelineSuspensionState.Resume }, 
+            new[] { HierarchicalBehavior.PropagateSuspendOnly, HierarchicalBehavior.PropagateSuspendOnly }, 
+            PipelineSuspensionState.Resume,
+            TestName = "PropagateSuspendOnly: Resume => ResumeWithoutPendingInvalidates => Resume = Resume")]
+        [TestCase(
+            PipelineSuspensionState.ResumeWithoutPendingInvalidates,
+            new[] { PipelineSuspensionState.Resume, PipelineSuspensionState.Resume },
+            new[] { HierarchicalBehavior.PropagateSuspendOnly, HierarchicalBehavior.PropagateSuspendOnly },
+            PipelineSuspensionState.Resume,
+            TestName = "PropagateSuspendOnly: ResumeWithoutPendingInvalidates => Resume => Resume = Resume")]
+
+        [TestCase(
+            PipelineSuspensionState.Resume,
+            new[] { PipelineSuspensionState.ResumeWithoutPendingInvalidates, PipelineSuspensionState.ResumeWithoutPendingInvalidates },
+            new[] { HierarchicalBehavior.PropagateSuspendOnly, HierarchicalBehavior.PropagateSuspendOnly },
+            PipelineSuspensionState.ResumeWithoutPendingInvalidates,
+            TestName = "PropagateSuspendOnly: Resume => ResumeWithoutPendingInvalidates => ResumeWithoutPendingInvalidates = ResumeWithoutPendingInvalidates")]
+        [TestCase(
+            PipelineSuspensionState.ResumeWithoutPendingInvalidates,
+            new[] { PipelineSuspensionState.Resume, PipelineSuspensionState.ResumeWithoutPendingInvalidates },
+            new[] { HierarchicalBehavior.PropagateSuspendOnly, HierarchicalBehavior.PropagateSuspendOnly },
+            PipelineSuspensionState.ResumeWithoutPendingInvalidates,
+            TestName = "PropagateSuspendOnly: ResumeWithoutPendingInvalidates => Resume => ResumeWithoutPendingInvalidates = ResumeWithoutPendingInvalidates")]
+
+
+        [TestCase(
+            PipelineSuspensionState.ResumeWithoutPendingInvalidates,
+            new[] { PipelineSuspensionState.Resume, PipelineSuspensionState.Resume },
+            new[] { HierarchicalBehavior.WeakenSuspensionState, HierarchicalBehavior.WeakenSuspensionState },
+            PipelineSuspensionState.ResumeWithoutPendingInvalidates,
+            TestName = "WeakenSuspensionState: ResumeWithoutPendingInvalidates => Resume => Resume = ResumeWithoutPendingInvalidates")]
+        [TestCase(
+            PipelineSuspensionState.Resume,
+            new[] { PipelineSuspensionState.ResumeWithoutPendingInvalidates, PipelineSuspensionState.Resume },
+            new[] { HierarchicalBehavior.WeakenSuspensionState, HierarchicalBehavior.WeakenSuspensionState },
+            PipelineSuspensionState.ResumeWithoutPendingInvalidates,
+            TestName = "WeakenSuspensionState: Resume => ResumeWithoutPendingInvalidates => Resume = ResumeWithoutPendingInvalidates")]
+
+        [TestCase(
+            PipelineSuspensionState.Suspend,
+            new[] { PipelineSuspensionState.Resume, PipelineSuspensionState.Resume },
+            new[] { HierarchicalBehavior.WeakenSuspensionState, HierarchicalBehavior.WeakenSuspensionState },
+            PipelineSuspensionState.Suspend,
+            TestName = "WeakenSuspensionState: Suspend => Resume => Resume = Suspend")]
+        [TestCase(
+            PipelineSuspensionState.Resume,
+            new[] { PipelineSuspensionState.ResumeWithoutPendingInvalidates, PipelineSuspensionState.Suspend },
+            new[] { HierarchicalBehavior.WeakenSuspensionState, HierarchicalBehavior.WeakenSuspensionState },
+            PipelineSuspensionState.Suspend,
+            TestName = "WeakenSuspensionState: Resume => ResumeWithoutPendingInvalidates => Suspend = Suspend")]
+
+        public void HierarchyPropagateSuspensionStateAsExpected(
+            PipelineSuspensionState root, 
+            PipelineSuspensionState[] hierarchy, 
+            HierarchicalBehavior[] behaviors, 
+            PipelineSuspensionState expectedFinalState)
+        {
+            var parent = new HierarchicalSuspenderNode(root.AsPipelineConstant());
+            var lastStage = hierarchy.Zip(behaviors, (a, b) => (input: a, behavior: b)).Aggregate(parent, (acc, next) => acc.CreateChild(next.input.AsPipelineConstant(), next.behavior));
+
+            PipelineAssert.Value(lastStage.Output, expectedFinalState);
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(4)]
+        public void InvalidationPropagatesThroughEntireHierarchy(int size)
+        {
+            var inputs = Enumerable.Repeat(PipelineSuspensionState.Resume, size + 1).Select(r => PipelineTestHelper.Assignable(r)).ToList();
+
+            var stages = new List<HierarchicalSuspenderNode> { new HierarchicalSuspenderNode(inputs[0]) };
+
+            for (int i = 0; i < size; ++i)
+                stages.Add(stages[i].CreateChild(inputs[i + 1], HierarchicalBehavior.WeakenSuspensionState));
+
+            var tests = stages.Select(s => s.Output.AttachTestStage()).ToList();
+
+            for(int i = 0; i < size; ++i)
+            {
+                inputs[i].SetValue(PipelineSuspensionState.Suspend);
+                foreach(var test in tests.Skip(i))
+                    test.AssertInvalidations(i + 1);
+                PipelineAssert.Value(stages.Last().Output, PipelineSuspensionState.Suspend);
+            }
+        }
+
+        [TestCase(PipelineSuspensionState.Resume, true)]
+        [TestCase(PipelineSuspensionState.ResumeWithoutPendingInvalidates, true)]
+        [TestCase(PipelineSuspensionState.Suspend, false)]
+        public void CreatingSuspenderStageFromHierarchyWillGetHierarchySuspensionState(PipelineSuspensionState state, bool expectInvalidation)
+        {
+            var hierarchy = new HierarchicalSuspenderNode(state.AsPipelineConstant());
+
+            var input = PipelineTestHelper.Assignable(1);
+            var sut = hierarchy.WithSuspender(input);
+            var test = sut.AttachTestStage();
+
+            input.SetValue(10);
+
+            if (expectInvalidation)
+                test.AssertInvalidations(1);
+            else
+                test.AssertInvalidations(0);
+        }
+    }
+}
---
+++ Viking.Pipeline/Patterns/HierarchicalSuspenderNode.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Viking.Pipeline.Patterns
+{
+    /// <summary>
+    /// Describes the expected hierarchical propagation behavior of a <see cref="HierarchicalSuspenderNode"/>.
+    /// </summary>
+    public enum HierarchicalBehavior
+    {
+        /// <summary>
+        /// Propagate suspension down to children. Any resume state simply takes the childs state instead.
+        /// </summary>
+        PropagateSuspendOnly,
+        /// <summary>
+        /// The childs state cannot be "stronger" that the parent's state. 
+        /// <see cref="PipelineSuspensionState.Resume"/> > <see cref="PipelineSuspensionState.ResumeWithoutPendingInvalidates"/> > <see cref="PipelineSuspensionState.Suspend"/>
+        /// </summary>
+        WeakenSuspensionState
+    }
+
+    /// <summary>
+    /// Creates a hierarchy of suspenders
+    /// </summary>
+    public sealed class HierarchicalSuspenderNode
+    {
+        /// <summary>
+        /// Creates a new <see cref="HierarchicalSuspenderNode"/> with the specified input as initial input.
+        /// </summary>
+        /// <param name="input"></param>
+        public HierarchicalSuspenderNode(IPipelineStage<PipelineSuspensionState> input)
+        {
+            Output = input;
+        }
+
+        internal HierarchicalSuspenderNode(IPipelineStage<PipelineSuspensionState> parent, IPipelineStage<PipelineSuspensionState> input, HierarchicalBehavior behavior)
+        {
+            if (behavior == HierarchicalBehavior.WeakenSuspensionState)
+                Output = PipelineOperations.Create("Hierarchical fusion (weaken suspension)", GetWeakestState, parent, input);
+            else
+                Output = PipelineOperations.Create("Hierarchical fusion (propagate suspend)", PropagateSuspensionOnly, parent, input);
+        }
+
+        /// <summary>
+        /// Gets the output pipeline of the hierarchy.
+        /// </summary>
+        public IPipelineStage<PipelineSuspensionState> Output { get; }
+
+        /// <summary>
+        /// Creates a child to this hierarchy node.
+        /// </summary>
+        /// <param name="input">The input suspension state of the child.</param>
+        /// <param name="behavior">The desired behavior.</param>
+        /// <returns>The child hierarchy.</returns>
+        public HierarchicalSuspenderNode CreateChild(IPipelineStage<PipelineSuspensionState> input, HierarchicalBehavior behavior)
+        {
+            return new HierarchicalSuspenderNode(Output, input, behavior);
+        }
+
+        /// <summary>
+        /// Adds the suspender.
+        /// </summary>
+        /// <typeparam name="TValue">The value type of the stage.</typeparam>
+        /// <param name="input">The stage.</param>
+        /// <returns>The suspended stage.</returns>
+        public IPipelineStage<TValue> WithSuspender<TValue>(IPipelineStage<TValue> input) => new SuspendingPipelineStage<TValue>(input, Output);
+
+        /// <summary>
+        /// Gets the weakest state of two.
+        /// </summary>
+        /// <param name="parent">The parent state.</param>
+        /// <param name="child">The child state.</param>
+        /// <returns>The weakest state, in the order Resume > ResumeWithoutInvalidate > Suspend.</returns>
+        public static PipelineSuspensionState GetWeakestState(PipelineSuspensionState parent, PipelineSuspensionState child)
+        {
+            switch (parent)
+            {
+                case PipelineSuspensionState.Resume:
+                    return child;
+                case PipelineSuspensionState.ResumeWithoutPendingInvalidates:
+                    return child == PipelineSuspensionState.Resume ? PipelineSuspensionState.ResumeWithoutPendingInvalidates : child;
+                case PipelineSuspensionState.Suspend:
+                    return PipelineSuspensionState.Suspend;
+                default:
+                    throw new ArgumentException("Invalid argument.", nameof(parent));
+            }
+        }
+
+        /// <summary>
+        /// Gets the child state, unless parent is suspended.
+        /// </summary>
+        /// <param name="parent">The parent state.</param>
+        /// <param name="child">The child state.</param>
+        /// <returns><see cref="PipelineSuspensionState.Suspend"/> if parent is suspended, otherwise the child state.</returns>
+        public static PipelineSuspensionState PropagateSuspensionOnly(PipelineSuspensionState parent, PipelineSuspensionState child)
+        {
+            if (parent == PipelineSuspensionState.Suspend)
+                return PipelineSuspensionState.Suspend;
+            return child;
+        }
+    }
+}
