8f490058797fea16cb32734d0e3dfb752ffdc869
Erik Nyström <grevor.priest@gmail.com>
2019-09-12 19:40:01 +0200

    Added a thread-safe option to the pipeline, ensuring only one thread can access its GetValue function at a time.

---
+++ Viking.Pipeline.Tests/ThreadSafePipelineStageTests.cs
@@ -0,0 +1,108 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Viking.Pipeline.Tests
+{
+    [TestFixture]
+    public class ThreadSafePipelineStageTests
+    {
+        [Test]
+        public void ExceptionOnNullInputToConstructor()
+        {
+            PipelineAssert.NullArgumentException(() => new ThreadSafePipelineStage<int>(null), "input");
+        }
+
+        [Test]
+        public void InputIsRegisteredAsDependency()
+        {
+            var input = 1.AsPipelineConstant();
+            var sut = new ThreadSafePipelineStage<int>(input);
+
+            PipelineAssert.DependentOn(sut, input);
+        }
+
+        [Test]
+        public void InvalidationsArePropagated()
+        {
+            var sut = new ThreadSafePipelineStage<int>(1.AsPipelineConstant());
+            var test = sut.AttachTestStage();
+
+            PipelineCore.Invalidate(sut);
+
+            test.AssertInvalidations(1);
+        }
+
+        [Test]
+        [Repeat(3)]
+        public void ConcurrentRetrievalsRetrievesOnlyOnceFromUpstreamStage()
+        {
+            using (EventWaitHandle completeRetrievalFlag = new EventWaitHandle(false, EventResetMode.ManualReset))
+            {
+                using (var semaphore = new SemaphoreSlim(0))
+                {
+                    var input = new DataRetrievalPipelineStage<object>("", () => { completeRetrievalFlag.WaitOne(); return new object(); });
+                    var inputTest = input.AttachTestStage();
+
+                    var sut = new ThreadSafePipelineStage<object>(inputTest);
+
+                    object GetObject()
+                    {
+                        semaphore.Release();
+                        return sut.GetValue();
+                    }
+
+                    var task1 = Task.Run(GetObject);
+                    var task2 = Task.Run(GetObject);
+
+                    var oneSecond = TimeSpan.FromSeconds(1);
+                    Assert.IsTrue(semaphore.Wait(oneSecond));
+                    Assert.IsTrue(semaphore.Wait(oneSecond));
+
+                    completeRetrievalFlag.Set();
+
+                    Assert.IsTrue(Task.WaitAll(new[] { task1, task2 }, oneSecond));
+
+                    var result1 = task1.Result;
+                    var result2 = task2.Result;
+
+                    Assert.AreSame(result1, result2);
+                }
+            }
+        }
+
+        [Test]
+        [Repeat(3)]
+        public void OnlyOneThreadIsInsideThePreviousPipelineStagesGetValueFunctionAtATime()
+        {
+            var container = new Container();
+            int EnsureOnlyOne()
+            {
+                if (container.Thread != null)
+                    throw new Exception();
+
+                container.Thread = Thread.CurrentThread;
+
+                if(container.Thread != Thread.CurrentThread)
+                    throw new Exception();
+
+                container.Thread = null;
+                return 1;
+            }
+
+            var input = new DataRetrievalPipelineStage<int>("", EnsureOnlyOne);
+            var sut = new ThreadSafePipelineStage<int>(input);
+            var tasks = Enumerable.Repeat(0, 1000).Select(_ => Task.Run(sut.GetValue)).ToArray();
+
+            Task.WaitAll(tasks);
+            Assert.Pass("No exceptions were thrown, indicating that no concurrency issues were found.");
+        }
+
+        private class Container
+        {
+            public volatile Thread Thread;
+        }
+    }
+}
---
+++ Viking.Pipeline/ThreadSafePipelineStage.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Viking.Pipeline
+{
+    /// <summary>
+    /// Makes a pipeline stage thread-safe, ensuring only one invocation of GetValue can be done at a time.
+    /// In case of concurrent requests, all requests will take the result of the currently running invocation.
+    /// </summary>
+    /// <typeparam name="TOutput">The output type.</typeparam>
+    public class ThreadSafePipelineStage<TOutput> : IPipelineStage<TOutput>
+    {
+        /// <summary>
+        /// Creates a new <see cref="ThreadSafePipelineStage{TOutput}"/> with the specified input to make thread-safe.
+        /// </summary>
+        /// <param name="input">The pipeline stage to make thread-safe.</param>
+        public ThreadSafePipelineStage(IPipelineStage<TOutput> input)
+        {
+            Input = input ?? throw new ArgumentNullException(nameof(input));
+            Name = "Thread safe: " + Input.Name;
+            this.AddDependencies(Input);
+        }
+
+        /// <summary>
+        /// The input to make thread-safe.
+        /// </summary>
+        public IPipelineStage<TOutput> Input { get; }
+        public string Name { get; }
+
+        private Task<TOutput> _currentlyRunningTask;
+
+        public TOutput GetValue()
+        {
+            var task = new Task<TOutput>(GetValueThreadSafe);
+            var runningTask = Interlocked.CompareExchange(ref _currentlyRunningTask, task, null);
+
+            if (runningTask != null)
+                return WaitForRunningInvocationAndGetResultFromIt(runningTask);
+            else
+                return InvokeGetValueAndThenCleanUp(task);
+        }
+
+        private static TOutput WaitForRunningInvocationAndGetResultFromIt(Task<TOutput> runningTask) => runningTask.Result;
+
+        private TOutput InvokeGetValueAndThenCleanUp(Task<TOutput> task)
+        {
+            try
+            {
+                task.RunSynchronously();
+                return task.Result;
+            }
+            finally
+            {
+                if (Interlocked.Exchange(ref _currentlyRunningTask, null) != task)
+                    throw new InvalidOperationException("Race condition detected: The task was exchanged during the operation.");
+            }
+        }
+
+        private TOutput GetValueThreadSafe() => Input.GetValue();
+
+        public void OnInvalidate(IPipelineInvalidator invalidator) => invalidator.InvalidateAllDependentStages(this);
+    }
+}
