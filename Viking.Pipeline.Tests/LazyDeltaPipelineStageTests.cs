8e916f6c0dd4ed5026e0a0cf9700b6bf6ae62d6f
Erik Nyström <grevor.priest@gmail.com>
2019-10-31 22:42:23 +0100

    Updated version number and added one more stage.

---
+++ Viking.Pipeline.Tests/LazyDeltaPipelineStageTests.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+
+namespace Viking.Pipeline.Tests
+{
+    [TestFixture]
+    public class LazyDeltaPipelineStageTests
+    {
+        [Test]
+        public void ExceptionOnNullInputToConstructor()
+        {
+            var valid = 1.AsPipelineConstant();
+            PipelineAssert.NullArgumentException(() => new LazyDeltaPipelineStage<int, int>(null, Extractor, valid), "name");
+            PipelineAssert.NullArgumentException(() => new LazyDeltaPipelineStage<int, int>("", null, valid), "extractor");
+            PipelineAssert.NullArgumentException(() => new LazyDeltaPipelineStage<int, int>("", Extractor, null), "input");
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(-20)]
+        public void InitialBaselineValueIsUsedInInitialDeltaEvaluation(int initial)
+        {
+            var assignable = PipelineTestHelper.Assignable(initial);
+            var sut = new LazyDeltaPipelineStage<int, int>("", Extractor, assignable);
+            PipelineAssert.Value(sut, 0);
+        }
+
+        [TestCase(10, 11, 1)]
+        [TestCase(-10, 34, 44)]
+        [TestCase(1, -2, -3)]
+        public void ExtractorIsUsedToCalculateNewDelta(int old, int @new, int expected)
+        {
+            var input = PipelineTestHelper.Assignable(old);
+            var sut = new LazyDeltaPipelineStage<int, int>("", Extractor, input);
+            var test = sut.AttachTestStage();
+
+            input.SetValue(@new);
+            test.AssertInvalidations(1);
+            PipelineAssert.Value(sut, expected);
+        }
+
+        [Test]
+        public void InvalidationIsPropagatedWithoutAnyGetValueBeingCalled()
+        {
+            var assignable = PipelineTestHelper.Assignable(0);
+            var test = assignable.AttachTestStage();
+            var sut = new LazyDeltaPipelineStage<int, int>("", Extractor, test);
+            var sutTest = sut.AttachTestStage();
+
+            test.AssertInvalidationsAndRetrievals(0, 1);
+
+            assignable.Invalidate();
+            test.AssertInvalidationsAndRetrievals(1, 1);
+            sutTest.AssertInvalidationsAndRetrievals(1, 0);
+        }
+
+        [Test]
+        public void InputIsAddedAsDependency()
+        {
+            var input = 1.AsPipelineConstant();
+            var sut = new DeltaPipelineStage<int, int>("", Extractor, input, -2);
+
+            PipelineAssert.DependentOn(sut, input);
+        }
+
+
+
+        private static int Extractor(int old, int @new) => @new - old;
+    }
+}
---
+++ Viking.Pipeline/LazyDeltaPipelineStage.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Viking.Pipeline
+{
+    /// <summary>
+    /// Supports lazy delta extraction. The delta and baseline is only recalculated when someone requests the value. 
+    /// </summary>
+    /// <typeparam name="TInput">The input type.</typeparam>
+    /// <typeparam name="TOutput">The delta type.</typeparam>
+    public sealed class LazyDeltaPipelineStage<TInput, TOutput> : IPipelineStage<TOutput>
+    {
+        /// <summary>
+        /// Creates a new <see cref="LazyDeltaPipelineStage{TInput, TOutput}"/> with the specified name, extractor and input.
+        /// </summary>        
+        /// <param name="name">The name of this stage.</param>
+        /// <param name="extractor">The function used to extract a delta.</param>
+        /// <param name="input">The pipeline stage which to extract delta from.</param>
+        public LazyDeltaPipelineStage(
+            string name,
+            DeltaExtractor<TInput, TOutput> extractor,
+            IPipelineStage<TInput> input)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Extractor = extractor ?? throw new ArgumentNullException(nameof(extractor));
+            Input = input ?? throw new ArgumentNullException(nameof(input));
+            OldValue = input.GetValue();
+            CurrentDelta = default!;
+            DeltaIsValid = false;
+            this.AddDependencies(input);
+        }
+
+        public string Name { get; }
+
+        private DeltaExtractor<TInput, TOutput> Extractor { get; }
+        private IPipelineStage<TInput> Input { get; }
+
+        private bool DeltaIsValid { get; set; }
+        private TInput OldValue { get; set; }
+        private TOutput CurrentDelta { get; set; }
+
+        public TOutput GetValue()
+        {
+            RecalculateDeltaIfNeeded();
+            return CurrentDelta;
+        }
+
+        public void OnInvalidate(IPipelineInvalidator invalidator)
+        {
+            DeltaIsValid = false;
+            invalidator.InvalidateAllDependentStages(this);
+        }
+
+        private void RecalculateDeltaIfNeeded()
+        {
+            if (DeltaIsValid)
+                return;
+
+            var newValue = Input.GetValue();
+            CurrentDelta = Extractor(OldValue, newValue);
+            OldValue = newValue;
+            DeltaIsValid = true;
+        }
+    }
+}
