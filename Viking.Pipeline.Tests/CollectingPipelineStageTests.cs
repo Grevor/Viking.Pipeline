19db74a4d5ca21612f0a6586e2e7a5201d22ee05
Erik Nyström <grevor.priest@gmail.com>
2019-12-07 13:59:46 +0100

    Added a CollectingPipelineStage which collates a number of pipeline stages to one enumerable.

---
+++ Viking.Pipeline.Tests/CollectingPipelineStageTests.cs
@@ -0,0 +1,103 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Viking.Pipeline.Tests
+{
+    [TestFixture]
+    public class CollectingPipelineStageTests
+    {
+        public void NullInputToConstructorThrowsException()
+        {
+            PipelineAssert.NullArgumentException(() => new CollectingPipelineStage<int>(null, Enumerable.Empty<IPipelineStage<int>>()), "Name");
+            PipelineAssert.NullArgumentException(() => new CollectingPipelineStage<int>("stage name", null), "initial inputs");
+            PipelineAssert.NullArgumentException(() => new CollectingPipelineStage<int>("stage name", new IPipelineStage<int>[] { null }), "initial input pipeline null");
+        }
+
+        [Test]
+        public void AddingInputInvalidatesStage()
+        {
+            var a = 1.AsPipelineConstant();
+            var sut = new CollectingPipelineStage<int>("", new[] { a });
+            var test = sut.AttachTestStage();
+
+            sut.AddInputs(new[] { 2.AsPipelineConstant() });
+
+            test.AssertInvalidations(1);
+        }
+
+        [Test]
+        public void AddingSameInputAgainDoesNotInvalidateStage()
+        {
+            var a = 1.AsPipelineConstant();
+            var sut = new CollectingPipelineStage<int>("", new[] { a });
+            var test = sut.AttachTestStage();
+
+            sut.AddInputs(new[] { a });
+
+            test.AssertInvalidations(0);
+        }
+
+        [Test]
+        public void RemovingInputInvalidatesStage()
+        {
+            var a = 1.AsPipelineConstant();
+            var sut = new CollectingPipelineStage<int>("", new[] { a });
+            var test = sut.AttachTestStage();
+
+            sut.RemoveInputs(new[] { a });
+
+            test.AssertInvalidations(1);
+        }
+
+        [Test]
+        public void RemovingNonInputDoesNotInvalidateStage()
+        {
+            var a = 1.AsPipelineConstant();
+            var sut = new CollectingPipelineStage<int>("", new[] { a });
+            var test = sut.AttachTestStage();
+
+            sut.RemoveInputs(new[] { 2.AsPipelineConstant() });
+
+            test.AssertInvalidations(0);
+        }
+
+        [Test]
+        public void InvalidationOfInputsInvalidatesStage()
+        {
+            var a = PipelineTestHelper.Assignable(1);
+            var sut = new CollectingPipelineStage<int>("", new[] { a });
+            var test = sut.AttachTestStage();
+
+            a.Invalidate();
+            test.AssertInvalidations(1);
+        }
+
+        [Test]
+        public void InvalidationOfAddedInputsInvalidatesStage()
+        {
+            var a = PipelineTestHelper.Assignable(1);
+            var b = PipelineTestHelper.Assignable(2);
+            var sut = new CollectingPipelineStage<int>("", new[] { a });
+            sut.AddInputs(new[] { b });
+            var test = sut.AttachTestStage();
+
+            b.Invalidate();
+            test.AssertInvalidations(1);
+        }
+
+        [Test]
+        public void InvalidationOfRemovedInputDoesNotInvalidateTheStage()
+        {
+            var a = PipelineTestHelper.Assignable(1);
+            var sut = new CollectingPipelineStage<int>("", new[] { a });
+            sut.RemoveDependencies(new[] { a });
+            var test = sut.AttachTestStage();
+
+            a.Invalidate();
+            test.AssertInvalidations(0);
+        }
+    }
+}
---
+++ Viking.Pipeline/CollectingPipelineStage.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Viking.Pipeline
+{
+    /// <summary>
+    /// Pipeline stage aggregating outputs from multiple stages into a single <see cref="IEnumerable{T}"/>.
+    /// </summary>
+    /// <typeparam name="TOutput">The output type.</typeparam>
+    public sealed class CollectingPipelineStage<TOutput> : IPipelineStage<IEnumerable<TOutput>>
+    {
+        /// <summary>
+        /// Creates a new <see cref="CollectingPipelineStage{TOutput}"/> with the specified name and initial inputs.
+        /// </summary>
+        /// <param name="name">The name of this stage.</param>
+        /// <param name="inputs">The initial inputs.</param>
+        public CollectingPipelineStage(string name, IEnumerable<IPipelineStage<TOutput>> inputs)
+        {
+            if (inputs is null)
+                throw new ArgumentNullException(nameof(inputs));
+
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Inputs = inputs.ToList();
+            this.AddDependencies(Inputs.ToArray());
+        }
+
+        public string Name { get; }
+        private List<IPipelineStage<TOutput>> Inputs { get; }
+        /// <summary>
+        /// Gets all current input <see cref="IPipelineStage{TOutput}"/>.
+        /// </summary>
+        public IEnumerable<IPipelineStage<TOutput>> CurrentInputs => Inputs;
+
+        /// <summary>
+        /// Removes the specified inputs from this stage.
+        /// </summary>
+        /// <param name="inputs">The inputs to remove.</param>
+        public void RemoveInputs(IEnumerable<IPipelineStage<TOutput>> inputs)
+        {
+            var stagesToRemove = inputs.Intersect(Inputs).ToArray();
+            foreach (var input in stagesToRemove)
+                Inputs.Remove(input);
+            this.RemoveDependencies(stagesToRemove);
+
+            if (stagesToRemove.Length > 0)
+                this.Invalidate();
+        }
+
+        /// <summary>
+        /// Adds the specified inputs from this stage.
+        /// </summary>
+        /// <param name="inputs">The inputs to add.</param>
+        public void AddInputs(IEnumerable<IPipelineStage<TOutput>> inputs)
+        {
+            var stagesToAdd = inputs.Except(Inputs).ToArray();
+            Inputs.AddRange(stagesToAdd);
+            this.AddDependencies(stagesToAdd);
+
+            if (stagesToAdd.Length > 0)
+                this.Invalidate();
+        }
+
+        public IEnumerable<TOutput> GetValue() => Inputs.Select(pipeline => pipeline.GetValue()).ToList();
+
+        public void OnInvalidate(IPipelineInvalidator invalidator) => invalidator.InvalidateAllDependentStages(this);
+    }
+}
