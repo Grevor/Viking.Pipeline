97367855ccdc6ca4e51862716bf3d5150db9b4dc
Erik Nyström <grevor.priest@gmail.com>
2020-03-03 22:29:48 +0100

    Added a multiplexing pipeline stage.

---
+++ Viking.Pipeline.Tests/MultiplexingPipelineStageTests.cs
@@ -0,0 +1,249 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Viking.Pipeline.Tests
+{
+    [TestFixture]
+    public class MultiplexingPipelineStageTests
+    {
+        [Test]
+        public void ExceptionOnNullInputToConstructor()
+        {
+            var name = "Name";
+            var select = 1.AsPipelineConstant();
+            var inputs = new Dictionary<int, IPipelineStage<int>> { };
+            var comparer = EqualityComparer<int>.Default;
+
+            PipelineAssert.NullArgumentException(() => new MultiplexingPipelineStage<int, int>(null, select, inputs), "name");
+            PipelineAssert.NullArgumentException(() => new MultiplexingPipelineStage<int, int>(null, select, inputs, comparer), "name");
+
+            PipelineAssert.NullArgumentException(() => new MultiplexingPipelineStage<int, int>(name, null, inputs), "select");
+            PipelineAssert.NullArgumentException(() => new MultiplexingPipelineStage<int, int>(name, null, inputs, comparer), "select");
+
+            PipelineAssert.NullArgumentException(() => new MultiplexingPipelineStage<int, int>(name, select, null), "inputs");
+            PipelineAssert.NullArgumentException(() => new MultiplexingPipelineStage<int, int>(name, select, null, comparer), "inputs");
+
+            PipelineAssert.NullArgumentException(() => new MultiplexingPipelineStage<int, int>(name, select, inputs, null), "comparer");
+        }
+
+        [Test]
+        public void DependenciesToInputsAndSelectAreRegisteredCorrectly()
+        {
+            var select = 0.AsPipelineConstant();
+
+            var input1 = 2.AsPipelineConstant();
+            var input2 = 4.AsPipelineConstant();
+            var inputs = CreateInputs(input1, input2);
+
+            var sut = new MultiplexingPipelineStage<int, int>("Name", select, inputs);
+
+            PipelineAssert.DependentOn(sut, select, input1, input2);
+        }
+
+        [Test]
+        public void ExceptionIsThrownOnInvalidateWhenTheSelectedStageDoesNotExistInInputs()
+        {
+            var select = 0.AsPipelineConstant();
+            var inputs = new Dictionary<int, IPipelineStage<int>> { { 1, 1.AsPipelineConstant() } };
+
+            var sut = new MultiplexingPipelineStage<int, int>("Name", select, inputs);
+
+            Assert.Catch(() => sut.Invalidate());
+        }
+
+        [Test]
+        public void ExceptionIsThrownOnGetValueWhenTheSelectedStageDoesNotExistInInputs()
+        {
+            var select = 0.AsPipelineConstant();
+            var inputs = new Dictionary<int, IPipelineStage<int>> { { 1, 1.AsPipelineConstant() } };
+
+            var sut = new MultiplexingPipelineStage<int, int>("Name", select, inputs);
+
+            Assert.Catch(() => sut.GetValue());
+        }
+
+        [Test]
+        public void StageIsInvalidatedWhenSelectSignalChanges()
+        {
+            var select = AssignablePipelineStage.Create("Select", 0);
+            var inputs = CreateInputs(2.AsPipelineConstant(), 4.AsPipelineConstant());
+
+            var sut = new MultiplexingPipelineStage<int, int>("Name", select, inputs);
+            var test = sut.AttachTestStage();
+
+            select.SetValue(1);
+            select.SetValue(0);
+
+            test.AssertInvalidations(2);
+        }
+
+        [Test]
+        public void StageInvalidatesDependentStages()
+        {
+            var select = AssignablePipelineStage.Create("Select", 0);
+            var inputs = CreateInputs(2.AsPipelineConstant(), 4.AsPipelineConstant());
+
+            var sut = new MultiplexingPipelineStage<int, int>("Name", select, inputs);
+            PipelineAssert.AssertPipelineIsInvalidatingDependentStages(sut);
+        }
+
+        [Test]
+        public void StageIsInvalidatedWhenCurrentlySelectedInputChanges()
+        {
+            var select = AssignablePipelineStage.Create("Select", 0);
+            var input = AssignablePipelineStage.Create("Input", 0);
+            var inputs = CreateInputs(input);
+
+            var sut = new MultiplexingPipelineStage<int, int>("Name", select, inputs);
+            var test = sut.AttachTestStage();
+
+            input.SetValue(1);
+            test.AssertInvalidations(1);
+        }
+
+        [Test]
+        public void StageIsNotInvalidatedWhenAnUnselectedInputChanges()
+        {
+            var select = AssignablePipelineStage.Create("Select", 0);
+            var input0 = AssignablePipelineStage.Create("Input 0", 0);
+            var input1 = AssignablePipelineStage.Create("Input 1", 0);
+            var inputs = CreateInputs(input0, input1);
+
+            var sut = new MultiplexingPipelineStage<int, int>("Name", select, inputs);
+            var test = sut.AttachTestStage();
+
+            input1.SetValue(1);
+            test.AssertInvalidations(0);
+        }
+
+        [TestCase(0, new[] { 2, 3, 4 }, 2)]
+        [TestCase(1, new[] { 2, 3, 4 }, 3)]
+        [TestCase(2, new[] { 2, 3, 4 }, 4)]
+        [TestCase(0, new[] { 7 }, 7)]
+        public void OutputIsTakenFromTheSelectedInput(int selected, int[] inputValues, int expected)
+        {
+            var select = AssignablePipelineStage.Create("Select", selected);
+            var inputs = CreateInputs(inputValues.Select(i => i.AsPipelineConstant()).ToArray());
+
+            var sut = new MultiplexingPipelineStage<int, int>("Name", select, inputs);
+            PipelineAssert.Value(sut, expected);
+        }
+
+        [Test]
+        public void SettingANewInputForTheCurrentlySelectedInputCausesInvalidationAndGetsTheNewValue()
+        {
+            var select = AssignablePipelineStage.Create("Select", 0);
+            var input0 = AssignablePipelineStage.Create("Input 0", 0);
+            var input1 = AssignablePipelineStage.Create("Input 1", 0);
+            var newInput0 = AssignablePipelineStage.Create("Input 0 (new)", 100);
+            var inputs = CreateInputs(input0, input1);
+
+            var sut = new MultiplexingPipelineStage<int, int>("Name", select, inputs);
+            var test = sut.AttachTestStage();
+
+            sut.SetInput(0, newInput0);
+
+            test.AssertInvalidations(1);
+            PipelineAssert.Value(sut, 100);
+        }
+
+        [Test]
+        public void SettingANewInputForAnUnselectedInputCausesNoInvalidation()
+        {
+            var select = AssignablePipelineStage.Create("Select", 1);
+            var input0 = AssignablePipelineStage.Create("Input 0", 0);
+            var input1 = AssignablePipelineStage.Create("Input 1", 0);
+            var newInput0 = AssignablePipelineStage.Create("Input 0 (new)", 100);
+            var inputs = CreateInputs(input0, input1);
+
+            var sut = new MultiplexingPipelineStage<int, int>("Name", select, inputs);
+            var test = sut.AttachTestStage();
+
+            sut.SetInput(0, newInput0);
+
+            test.AssertInvalidations(0);
+        }
+
+        [Test]
+        public void RemovingTheCurrentlySelectedInputCausesException()
+        {
+            var select = AssignablePipelineStage.Create("Select", 0);
+            var input0 = AssignablePipelineStage.Create("Input 0", 0);
+            var inputs = CreateInputs(input0);
+
+            var sut = new MultiplexingPipelineStage<int, int>("Name", select, inputs);
+
+            Assert.Catch(() => sut.RemoveInput(0));
+        }
+
+        [Test]
+        public void RemovingAStageRemovesDependency()
+        {
+            var select = AssignablePipelineStage.Create("Select", 0);
+            var input0 = AssignablePipelineStage.Create("Input 0", 0);
+            var input1 = AssignablePipelineStage.Create("Input 1", 0);
+            var inputs = CreateInputs(input0, input1);
+
+            var sut = new MultiplexingPipelineStage<int, int>("Name", select, inputs);
+
+            sut.RemoveInput(1);
+            PipelineAssert.NotDependentOn(sut, input1);
+        }
+
+        [Test]
+        public void SettingANewMappingRemovesOldDependencyAndAddsNew()
+        {
+            var select = AssignablePipelineStage.Create("Select", 0);
+            var input0 = AssignablePipelineStage.Create("Input 0", 0);
+            var newInput0 = AssignablePipelineStage.Create("Input 0 (new)", 0);
+            var inputs = CreateInputs(input0);
+            
+            var sut = new MultiplexingPipelineStage<int, int>("Name", select, inputs);
+
+            sut.SetInput(0, newInput0);
+
+            PipelineAssert.NotDependentOn(sut, input0);
+            PipelineAssert.DependentOn(sut, newInput0);
+        }
+
+        [Test]
+        public void DependencyIsOnlyRemovedIfAllInputsDependentOnThatStageAreRemoved()
+        {
+            var select = AssignablePipelineStage.Create("Select", 0);
+            var input0 = AssignablePipelineStage.Create("Input 0", 0);
+            var input2 = AssignablePipelineStage.Create("Input 2", 0);
+            var inputs = CreateInputs(input0, input0, input2);
+            
+            var sut = new MultiplexingPipelineStage<int, int>("Name", select, inputs);
+
+            sut.RemoveInput(1);
+            PipelineAssert.DependentOn(sut, input0, input2);
+            select.SetValue(2); // To prevent exception
+            sut.RemoveInput(0);
+            PipelineAssert.NotDependentOn(sut, input0);
+            PipelineAssert.DependentOn(sut, input2);
+        }
+
+        [Test]
+        public void DependencyIsOnlyRemovedIfAllInputsDependentOnThatStageAreChanged()
+        {
+            var select = AssignablePipelineStage.Create("Select", 0);
+            var input0 = AssignablePipelineStage.Create("Input 0", 0);
+            var newInput0 = AssignablePipelineStage.Create("Input 0 (new)", 0);
+            var inputs = CreateInputs(input0, input0);
+
+            var sut = new MultiplexingPipelineStage<int, int>("Name", select, inputs);
+
+            sut.SetInput(0, newInput0);
+            PipelineAssert.DependentOn(sut, input0);
+            sut.SetInput(1, newInput0);
+            PipelineAssert.NotDependentOn(sut, input0);
+        }
+
+        private Dictionary<int, IPipelineStage<int>> CreateInputs(params IPipelineStage<int>[] inputs)
+            => Enumerable.Range(0, inputs.Length).ToDictionary(i => i, i => inputs[i]);
+    }
+}
---
+++ Viking.Pipeline/MultiplexingPipelineStage.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Viking.Pipeline
+{
+    /// <summary>
+    /// Selects one of multiple input signals to output.
+    /// </summary>
+    /// <typeparam name="TSelect">The select signal type.</typeparam>
+    /// <typeparam name="TOutput">The output type.</typeparam>
+    public class MultiplexingPipelineStage<TSelect, TOutput> : IPipelineStage<TOutput>
+    {
+        /// <summary>
+        /// Creates a new <see cref="MultiplexingPipelineStage{TSelect, TOutput}"/> with the given select signal, inputs and select signal comparer.
+        /// </summary>
+        /// <param name="name">The name of this pipeline stage.</param>
+        /// <param name="select">The select signal.</param>
+        /// <param name="inputs">The input stages.</param>
+        public MultiplexingPipelineStage(
+            string name,
+            IPipelineStage<TSelect> select,
+            IDictionary<TSelect, IPipelineStage<TOutput>> inputs)
+            : this(name, select, inputs, EqualityComparer<TSelect>.Default)
+        { }
+
+        /// <summary>
+        /// Creates a new <see cref="MultiplexingPipelineStage{TSelect, TOutput}"/> with the given select signal, inputs and select signal comparer.
+        /// </summary>
+        /// <param name="name">The name of this pipeline stage.</param>
+        /// <param name="select">The select signal.</param>
+        /// <param name="inputs">The input stages.</param>
+        /// <param name="comparer">The comparer used when comparing the select signals.</param>
+        public MultiplexingPipelineStage(
+            string name, 
+            IPipelineStage<TSelect> select, 
+            IDictionary<TSelect, IPipelineStage<TOutput>> inputs, 
+            IEqualityComparer<TSelect> comparer)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Select = select ?? throw new ArgumentNullException(nameof(select));
+            Comparer = comparer ?? throw new ArgumentNullException(nameof(comparer));
+            Inputs = new Dictionary<TSelect, IPipelineStage<TOutput>>(inputs, Comparer);
+
+            this.AddDependencies(Select);
+            this.AddDependencies(inputs.Values.ToArray());
+        }
+
+        public string Name { get; }
+        private IPipelineStage<TSelect> Select { get; }
+        private IEqualityComparer<TSelect> Comparer { get; }
+        private Dictionary<TSelect, IPipelineStage<TOutput>> Inputs { get; }
+
+        private TSelect CurrentSelectSignal => Select.GetValue();
+
+        private bool HasInput(IPipelineStage<TOutput> stage) => Inputs.Values.Contains(stage);
+        private IPipelineStage<TOutput> GetSelectedOutput()
+        {
+            return Inputs.TryGetValue(CurrentSelectSignal, out var selected)
+            ? selected
+            : throw new ArgumentOutOfRangeException(FormattableString.Invariant($"Did not find input to multiplex for select signal {CurrentSelectSignal}"));
+        }
+
+        private void Remove(TSelect key)
+        {
+            if (Inputs.TryGetValue(key, out var input))
+            {
+                Inputs.Remove(key);
+                if (!HasInput(input))
+                    this.RemoveDependencies(input);
+            }
+        }
+
+        /// <summary>
+        /// Adds or changes an input mapping.
+        /// </summary>
+        /// <param name="key">The key to associate with the specified input stage.</param>
+        /// <param name="input">The input stage.</param>
+        /// <exception cref="ArgumentNullException">If <paramref name="input"/> is null.</exception>
+        public void SetInput(TSelect key, IPipelineStage<TOutput> input)
+        {
+            if (input is null)
+                throw new ArgumentNullException(nameof(input));
+
+            Remove(key);
+
+            var inputAlreadyAddedAsDependency = HasInput(input);
+            Inputs.Add(key, input);
+
+            if (!inputAlreadyAddedAsDependency)
+                this.AddDependencies(input);
+
+            if (Comparer.Equals(key, CurrentSelectSignal))
+                this.Invalidate();
+        }
+
+        /// <summary>
+        /// Removes an input mapping.
+        /// </summary>
+        /// <param name="key">The key to remove mapping for.</param>
+        /// <exception cref="ArgumentException">If <paramref name="key"/> is currently the selected input.</exception>
+        public void RemoveInput(TSelect key)
+        {
+            if (Comparer.Equals(key, CurrentSelectSignal))
+                throw new ArgumentException("Cannot remove the currently selected input.");
+
+            Remove(key);
+        }
+
+        public TOutput GetValue() => GetSelectedOutput().GetValue();
+
+        public void OnInvalidate(IPipelineInvalidator invalidator)
+        {
+            var selfInvalidated = !invalidator.AllInvalidatedStages.Skip(1).Any() && invalidator.IsInvalidated(this);
+
+            var selectedInput = GetSelectedOutput();
+
+            if (invalidator.IsInvalidated(Select) || invalidator.IsInvalidated(selectedInput) || selfInvalidated)
+                invalidator.InvalidateAllDependentStages(this);
+            else
+                invalidator.Revalidate(this);
+        }
+    }
+}
